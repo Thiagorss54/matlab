eef864799e31ae3a52871fee63fc069989fa0d96
Gabriel Alves <gabrielalves98@hotmail.com>
2019-10-20 13:52:26 -0300

    Inicio Classe Matriz

---
+++ matlab.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "matlab.h"
+
+using namespace std;
+
+Matriz::Matriz(){
+
+}
+Matriz::Matriz(int linhas, int colunas, const double &valor = 0){
+
+}
+Matriz::Matriz(const Matriz &m){
+    
+}
+Matriz::~Matriz(){
+    
+}
+
---
+++ matlab.h
@@ -0,0 +1,24 @@
+#ifndef MATLAB_H
+#define MATLAB_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Matriz{
+	private:
+        vector<vector<double>> matriz;
+
+	public:
+        Matriz(); //cria uma matriz vazia com números de linhas e colunas iguais a 0
+        Matriz(int linhas, int colunas, const double &valor = 0); //cria uma matriz com
+            // número de linhas e colunas iguais aos recebidos pelos argumentos e com todos
+            // os valores da matriz preenchidos com a variável valor.
+        Matriz(const Matriz &m); //construtor de cópia
+        ~Matriz(); //destrutor
+
+};
+
+#endif //MATLAB_H
